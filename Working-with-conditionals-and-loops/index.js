d07e771665564e853844d1a9169de382f29b0584
jouvanprb <ovanpurba005@gmail.com>
2026-06-23 21:51:46 +0700

    add exercise 5: working with conditionals and loops

---
+++ Working-with-conditionals-and-loops/index.js
@@ -0,0 +1,31 @@
+// Task 1 Solution
+for (var i = 1; i <=10; i++) {
+    if (i == 1) {
+        console.log('Gold medal')
+    }
+    else if (i == 2) {
+        console.log('Silver medal')
+    }else if (i == 3) {
+        console.log('Bronze medal')
+    }else {
+        console.log(i)
+    }
+}
+
+
+// TAsk 2 Solution
+for (var i = 1; i <=10; i++) {
+    switch (i) {
+        case 1:
+            console.log("Gold Medal")
+            break    
+        case 2:
+            console.log("Silver Medal")
+            break
+        case 3:
+            console.log("Silver Medal")
+            break
+        default :
+            console.log(i)
+    }
+}
