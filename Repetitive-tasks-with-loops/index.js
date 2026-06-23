736a7b5d2646fd7ad5cefb4c0d08056811793d03
jouvanprb <ovanpurba005@gmail.com>
2026-06-23 21:40:11 +0700

    add exercise 4: Repetitive with loops

---
+++ Repetitive-tasks-with-loops/index.js
@@ -0,0 +1,84 @@
+// Task 1
+// Tulislah sebuah perulangan "for" yang akan melakukan kode berulang yang sama persis seperti ini:
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+console.log('Counting completed!')
+
+// My solution
+for (var i = 1; i < 6; i++) {
+    console.log(i);
+}
+console.log('Counting completed')
+
+
+// Task 2
+// Tuliskan sebuah perulangan "for" yang akan melakukan kode berulang yang sama persis seperti ini:
+console.log(5)
+console.log(4)
+console.log(3)
+console.log(2)
+console.log(1)
+console.log('Countdown finished!')
+
+// My solution
+for (var i = 5; i > 0; i--) {
+    console.log(i);
+}
+console.log('Counting completed')
+
+
+// Task 3
+// Tuliskan sebuah perulangan "while" yang akan melakukan kode berulang yang sama persis seperti ini:
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+console.log('Counting completed!')
+
+// My solution
+var i = 1
+
+while (i<6) {
+    console.log(i)
+    i++
+}
+console.log('Counting completed!')
+
+
+// Task 4
+// Tulis perulangan "while" yang akan melakukan kode berulang yang sama persis seperti ini:
+console.log(5)
+console.log(4)
+console.log(3)
+console.log(2)
+console.log(1)
+console.log('Countdown finished!')
+
+// My solution
+var i = 5
+
+while (i>0) {
+    console.log(i)
+    i--
+}
+console.log('Counting completed!')
+
+
+// Task 5
+// Tulis perulangan "while" yang akan melakukan kode berulang yang sama persis seperti ini:
+console.log(2018)
+console.log(2019)
+console.log(2020)
+console.log(2021)
+console.log(2022)
+
+// My solution
+var i = 2018
+
+while (i<2023) {
+    console.log(i++)
+}
